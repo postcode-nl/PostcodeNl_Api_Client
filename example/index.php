eeb6e987e713d78c6b699960d10c0b47f086273c
Geoffrey Dijkstra <geoffrey.dijkstra@gmail.com>
2020-06-03 17:12:30 +0200

    Made example PSR12 compliant.
    Made index.php in example directory as compliant as possible with PSR12 but because it does declare multiple symbols it's excluded from phpcs checking. Also removed exclude from phpcs.xml.

--- example/index.php
+++ example/index.php
@@ -1,41 +1,38 @@
 <?php
 
-define('API_KEY',    '** insert key here **');
+define('API_KEY', '** insert key here **');
 define('API_SECRET', '** insert secret here **');
-define('PLATFORM',   'example proxy');
-
+define('PLATFORM', 'example proxy');
 
 // this will load exceptions when they occur
-spl_autoload_register(function($class){
-	if (0 === strpos($class, 'PostcodeNl\\'))
-		require(__DIR__ .'/../src/'. str_replace('\\', '/', $class) .'.php');
-});
+require_once __DIR__ . '/../vendor/autoload.php';
 
 header('Content-Type: application/json');
 
-try
-{
-	if (!isset($_GET['p']))
-		throw new Exception('Missing parameters');
+try {
+    if (!isset($_GET['p'])) {
+        throw new Exception('Missing parameters');
+    }
 
-	$parts = explode('/', $_GET['p']);
+    $parts = explode('/', $_GET['p']);
 
-	if (count($parts) < 2)
-		throw new Exception('Not enough parameters');
+    if (count($parts) < 2) {
+        throw new Exception('Not enough parameters');
+    }
 
-	$action = array_shift($parts);
+    $action = array_shift($parts);
 
-	if (0 !== strpos($action, 'international') && 0 !== strpos($action, 'dutchAddress'))
-		throw new Exception('This example only supports calls to international or dutchAddress methods');
+    if (0 !== strpos($action, 'international') && 0 !== strpos($action, 'dutchAddress')) {
+        throw new Exception('This example only supports calls to international or dutchAddress methods');
+    }
 
-	if ($action == 'internationalAutocomplete' || $action == 'internationalGetDetails')
-		$parts []= 'MY_SESSION_ID';
+    if ($action == 'internationalAutocomplete' || $action == 'internationalGetDetails') {
+        $parts [] = 'MY_SESSION_ID';
+    }
 
-	$client = new PostcodeNl\Api\Client(API_KEY, API_SECRET, PLATFORM);
-	print json_encode(call_user_func_array([$client, $action], $parts));
-}
-catch (Exception $e)
-{
-	http_response_code(400);
-	print json_encode(['Exception' => $e->getMessage()]);
+    $client = new PostcodeNl\Api\Client(API_KEY, API_SECRET, PLATFORM);
+    print json_encode(call_user_func_array([$client, $action], $parts));
+} catch (Exception $e) {
+    http_response_code(400);
+    print json_encode(['Exception' => $e->getMessage()]);
 }
